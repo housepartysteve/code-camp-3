91469dad5ee662b4becd66372b3c782e2b23a1f3
STESPC\Stephen <steve2804@hotmail.co.uk>
2017-04-15 15:07:29 +0100

    layouts folder updated with relavant webpages

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,40 +1,27 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="online interactive environment">
-        <meta name="author" content="Stephen McCarrick">
-        <link rel="icon" href="#">
-
-        <title>{{ config('app.name', 'Code Camp') }}</title>
-
-        <!-- Bootstrap core CSS -->
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-        <!-- Custom styles for this template -->
-        <link href="{{ asset('css/app.css') }}"  rel="stylesheet">
-
-        <!-- Scripts -->
-        <script>
-            window.Laravel = {!! json_encode([
-            'csrfToken' => csrf_token(),
-        ]) !!};
-        </script>
+        <!-- Head -->
+        @include('includes.head')
     </head>
     <body>
-            <!-- Navigation Bar -->
-            @include('layouts.nav')
+    <div class="container">
 
-            <!-- Index content -->
+        <!-- Navigation Bar -->
+        <nav class="navbar navbar-default navbar-fixed-top">
+            @include('includes.nav')
+        </nav>
+
+        <!-- Index content -->
+        <div id="main" class="row">
             @yield('content')
+        </div>
 
-            <!-- Footer -->
-            @include('layouts.footer')
+        <!-- Footer -->
+        <footer class="row">
+            @include('includes.footer')
+        </footer>
 
-        <!-- Latest compiled and minified JS -->
-        <script src="{{ asset('js/app.js') }}"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </div>
     </body>
 </html>
